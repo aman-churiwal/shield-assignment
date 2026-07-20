fc18fa084e2386f7681ab9d0c591c198a868c8b2
Aman Churiwal <amanchuriwal22@gmail.com>
2026-07-20 18:26:20 +0530

    added models

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shield-assignment
 
 go 1.26.5
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ internal/model/model.go
@@ -0,0 +1,31 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type LoginEvent struct {
+	ID        uuid.UUID `json:"id"`
+	UserID    uuid.UUID `json:"user_id"`
+	LoginTime time.Time `json:"login_time"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type LoginRequest struct {
+	UserID    uuid.UUID `json:"user_id" binding:"required"`
+	LoginTime time.Time `json:"login_time" binding:"required"`
+}
+
+type DailyUniqueUsersResponse struct {
+	Date        string `json:"date"`
+	TimeZone    string `json:"timezone"`
+	UniqueUsers int    `json:"unique_users"`
+}
+
+type MonthlyUniqueUsersResponse struct {
+	Date        string `json:"date"`
+	TimeZone    string `json:"timezone"`
+	UniqueUsers int    `json:"unique_users"`
+}
